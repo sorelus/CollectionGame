ada544324d8553ea43d7cf4ba6dce25ac7e8252a
Clebert Sorel <c.mkounga.ndefo@accenture.com>
2019-06-14 15:22:17 +0400

    add error page

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Outils de developpement
 -----------------
  * Maven 3
  * Java 8 (spring boot version 2.0 (Spring Framework 5))
-
+ * Mysql 5.6
 
 Compilation et execution du projet
 -----------------
@@ -14,10 +14,21 @@ Compilation et execution du projet
 
 * Installer Maven 3 ( >3.2)
 
+* Installer Mysql 5.6
+    * Creer une DB : games_db
+    * Creer un utilsateur avec des access complets sur cette DB 
+    <br/>
+    <b>user </b>: admin
+    <br/>
+    <b>pass </b> :sorelus50 
+    <br/>
+    Vous pouvez configurer les access DB à utiliser par l'application depuis le fichier : <i>sorelTP2/src/main/resources/application.properties</i> 
+    
+    
 * Compilation
 	* cd sorelTP2
-	* mvn clean pakage
+	* mvn clean package
 
 * Executer
-	* java -jar target//sorel.tp2-0.0.1.jar
-
+	* java -jar target/sorel.tp2-0.0.3.war
+	* Ouvrez le navigateur et entrez http://localhost:8080
---
+++ sorelTP2/src/main/java/com/altima/validation/viewscontrollers/ErreurController.java
@@ -0,0 +1,31 @@
+package com.altima.validation.viewscontrollers;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class ErreurController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(Model model, HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        Integer statusCode = 0;
+        if (status != null)
+            statusCode = Integer.valueOf(status.toString());
+
+        model.addAttribute("code",statusCode);
+
+        return "/error";
+    }
+ 
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+}
